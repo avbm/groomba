c543b7114f35f4ff96b2b20ff2f7c04e00b0220e
Amod Mulay <amodm@splunk.com>
2020-09-20 08:36:13 -0400

    WIP

--- groomba.go
+++ groomba.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-
 	//"github.com/go-git/go-git/v5/storage/memory"
-	"log"
 )
 
+// CheckIfError should be used to naively panics if an error is not nil.
+func CheckIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Println("Hello World!")
-	r, _ := git.PlainOpen(".")
+	repo, _ := git.PlainOpen(".")
 	// rem, _ := r.Remote("origin")
 	//listBranches, err := rem.List()
-	listBranches, err := r.Branches()
-	if err != nil {
-		log.Fatal(err)
-	}
+	listBranches, err := repo.Branches()
+	CheckIfError(err)
 	listBranches.ForEach(func(ref *plumbing.Reference) error {
-		fmt.Println(ref)
+		// fmt.Println(ref)
+		commit, err := repo.CommitObject(ref.Hash())
+		CheckIfError(err)
+		fmt.Println(ref.Name().String(), commit.Author.Name, commit.Author.When)
 		return nil
 	})
 	// for _, branch := range listBranches {
