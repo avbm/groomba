09f0123e88f926f81e22406319ac5934c3487025
Amod Mulay <amodm@splunk.com>
2020-10-16 11:49:04 -0400

    Initial implementation with no tests

--- go.mod
+++ go.mod
@@ -1,8 +1,13 @@
 module groomba
 
-go 1.14
+go 1.15
 
 require (
 	github.com/go-git/go-git v4.7.0+incompatible // indirect
-	github.com/go-git/go-git/v5 v5.0.0
+	github.com/go-git/go-git/v5 v5.2.0
+	github.com/imdario/mergo v0.3.11 // indirect
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
--- groomba.go
+++ groomba.go
@@ -3,36 +3,131 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
-	//"github.com/go-git/go-git/v5/storage/memory"
+
+	"gopkg.in/yaml.v3"
 )
 
+const (
+	STALE_AGE_THRESHOLD = 3
+)
+
+var STATIC_BRANCHES = []string{"production", "staging", "canary", "govcloud", "release", "master", "gcp_staging", "gcp_beta", "gcp_production"}
+
 // CheckIfError should be used to naively panics if an error is not nil.
-func CheckIfError(err error) {
+func CheckIfError(err error, prefix ...string) {
 	if err == nil {
 		return
 	}
 
-	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("ERROR: %s %s", prefix, err))
 	os.Exit(1)
+}
+
+func isStaticBranch(name string) bool {
+	for _, b := range STATIC_BRANCHES {
+		if b == name {
+			return true
+		}
+	}
+	return false
+}
+
+func filterBranches(repo *git.Repository, threshold int) []*plumbing.Reference {
+	branchList, err := repo.References() //Branches()
+	CheckIfError(err)
+
+	filteredBranches := []*plumbing.Reference{}
+	branchList.ForEach(func(ref *plumbing.Reference) error {
+		// fmt.Println(ref)
+		// fmt.Println(ref.Name().String())
+		if ref.Type() == plumbing.HashReference && ref.Name().IsRemote() &&
+			!isStaticBranch(ref.Name().String()) &&
+			!strings.HasPrefix(ref.Name().String(), "refs/remotes/origin/revert") &&
+			!strings.HasPrefix(ref.Name().String(), "refs/remotes/origin/cherry-pick") &&
+			!strings.HasPrefix(ref.Name().String(), "refs/remotes/origin/stale") {
+
+			commit, err := repo.CommitObject(ref.Hash())
+			CheckIfError(err)
+
+			t, err := time.ParseDuration(fmt.Sprintf("%dh", threshold*24))
+			CheckIfError(err)
+			if time.Since(commit.Author.When) > t {
+				filteredBranches = append(filteredBranches, ref)
+			}
+		}
+		// fmt.Println(ref.Name().String(), commit.Author.Name, commit.Author.When)
+		return nil
+	})
+	return filteredBranches
+}
+
+func printBranchesGroupbyAuthor(repo *git.Repository, branches []*plumbing.Reference) {
+	type Branch struct {
+		Name string
+		Age  string
+	}
+	authors := make(map[string][]*Branch)
+	for _, ref := range branches {
+		commit, err := repo.CommitObject(ref.Hash())
+		CheckIfError(err)
+
+		b := &Branch{
+			Name: ref.Name().String(),
+			Age:  fmt.Sprintf("%dd", int64(time.Since(commit.Author.When).Hours()/24)),
+		}
+		if _, ok := authors[commit.Author.Name]; ok {
+			authors[commit.Author.Name] = append(authors[commit.Author.Name], b)
+		} else {
+			authors[commit.Author.Name] = []*Branch{b}
+		}
+
+	}
+
+	a, err := yaml.Marshal(authors)
+	CheckIfError(err)
+
+	fmt.Println(string(a))
+}
+
+func moveBranch(repo *git.Repository, ref *plumbing.Reference) error {
+	remote, err := repo.Remote("origin")
+	if err != nil {
+		return err
+	}
+	refName := ref.Name().String()
+	renameSpec := config.RefSpec(fmt.Sprintf("%s:stale/%s", refName, refName))
+	remote.Push(&git.PushOptions{RefSpecs: []config.RefSpec{renameSpec}})
+
+	deleteSpec := config.RefSpec(fmt.Sprintf(":%s", refName))
+	remote.Push(&git.PushOptions{RefSpecs: []config.RefSpec{deleteSpec}})
+	return nil
+}
+
+func moveStaleBranches(repo *git.Repository, branches []*plumbing.Reference) error {
+	for _, ref := range branches {
+		err := moveBranch(repo, ref)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
 	repo, _ := git.PlainOpen(".")
-	// rem, _ := r.Remote("origin")
-	//listBranches, err := rem.List()
-	listBranches, err := repo.Branches()
-	CheckIfError(err)
-	listBranches.ForEach(func(ref *plumbing.Reference) error {
-		// fmt.Println(ref)
-		commit, err := repo.CommitObject(ref.Hash())
-		CheckIfError(err)
-		fmt.Println(ref.Name().String(), commit.Author.Name, commit.Author.When)
-		return nil
+	repo.Fetch(&git.FetchOptions{
+		RemoteName: "origin",
+		RefSpecs:   []config.RefSpec{"refs/remotes/origin"},
+		Depth:      1,
 	})
-	// for _, branch := range listBranches {
-	// 	fmt.Println(branch)
-	// }
+	fb := filterBranches(repo, STALE_AGE_THRESHOLD)
+	printBranchesGroupbyAuthor(repo, fb)
+	fb = fb[:10]
+	moveStaleBranches(repo, fb)
 }
