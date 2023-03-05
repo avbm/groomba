98b19b028fa53f44e7872d7ff440f26ae80cbb31
Amod Mulay <amodmulay1@gmail.com>
2023-03-04 20:17:49 -0800

    add ssh-agent to test

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
@@ -17,6 +21,18 @@ func TestNewAuth(t *testing.T) {
 	})
 
 	t.Run("ssh-agent auth", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		cmd := exec.Command("ssh-agent", "-s")
+		cmd.Stdout = stdout
+		err := cmd.Run()
+		assert.Nil(t, err, "failed to setup ssh-agent for test")
+		for _, s := range strings.Split(stdout.String(), ";") {
+			if strings.Contains(s, "=") {
+				envVar := strings.Split(s, "=")
+				os.Setenv(envVar[0], envVar[1])
+			}
+		}
+
 		a, err := NewAuth(SSHAgentAuth)
 		assert.Nil(t, err)
 		_, ok := a.auth.(*ssh.PublicKeysCallback)
